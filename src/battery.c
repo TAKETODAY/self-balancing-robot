fa5fdbef04b0e64a2d6e4f40ae148f937fcf8b5e
海子 Yang <i.take.today@gmail.com>
2025-12-01 22:44:36 +0800

    :art:

--- src/battery.c
+++ src/battery.c
@@ -5,12 +5,12 @@
 #include <sys/unistd.h>
 
 #include "esp_log.h"
-#include "portmacro.h"
 #include "driver/gpio.h"
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_oneshot.h"
 #include "esp_adc/adc_cali_scheme.h"
 
+static const adc_channel_t channel = ADC_CHANNEL_7;
 
 const static char* TAG = "battery";
 
@@ -91,34 +91,19 @@ void showBatteryLED(void* pvParameters) {
 
 
 void battery_init() {
-  if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
-    printf("eFuse Two Point: Supported\n");
-  }
-  else {
-    printf("eFuse Two Point: NOT supported\n");
-  }
-  // Check Vref is burned into eFuse
-  if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
-    printf("eFuse Vref: Supported\n");
-  }
-  else {
-    printf("eFuse Vref: NOT supported\n");
-  }
-
-
   const adc_oneshot_unit_init_cfg_t init_config1 = {
     .unit_id = ADC_UNIT_1,
   };
   ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
 
   const adc_oneshot_chan_cfg_t config = {
-    .atten = atten,
+    .atten = ADC_ATTEN_DB_12,
     .bitwidth = ADC_BITWIDTH_DEFAULT,
   };
 
   ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, channel, &config));
 
-  do_calibration1_chan0 = _adc_calibration_init(ADC_UNIT_1, channel, atten, &adc_cali_handle);
+  do_calibration1_chan0 = _adc_calibration_init(ADC_UNIT_1, channel, ADC_ATTEN_DB_12, &adc_cali_handle);
 
   const gpio_config_t my_io_config = {
     .pin_bit_mask = 1 << LED_BATTERY_PIN,
--- src/battery.h
+++ src/battery.h
@@ -1,14 +1,7 @@
 #pragma once
 
-#include <esp_adc_cal.h>
-
 #define LED_BATTERY_PIN 13
 #define BAT_PIN 35
-
-static const adc1_channel_t channel = ADC1_CHANNEL_7;
-static const adc_bits_width_t width = ADC_WIDTH_BIT_12; // ADC采样宽度
-static const adc_atten_t atten = ADC_ATTEN_DB_12; // ADC衰减系数
-static const adc_unit_t unit = ADC_UNIT_1; // ADC单元
 
 void battery_init();
 
